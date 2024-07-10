0638cb45fb8b8c17aa959c128ae514ef00b5ad38
Scooter <talkufy@outlook.com>
2024-07-10 00:13:32 -0400

    Added stuff and some big fixes

--- venv/main.py
+++ venv/main.py
@@ -2,6 +2,8 @@ import constants
 import discord
 from discord.ext import commands
 import os
+import cog
+
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -13,18 +15,15 @@ async def on_ready():
     await bot.tree.sync(guild=discord.Object(id=constants.guildID))
     print(f'Logged in as {bot.user}')
 
-@bot.tree.command(name="hello", description="Say hello to the bot", guild=discord.Object(id=constants.guildID))
-async def hello(interaction: discord.Interaction):
-    await interaction.response.send_message("Hello!")
-
-for filename in os.listdir('cogs'):
-    if filename.endswith('.py'):
-        bot.load_extension(f'cogs.{filename[:-3]}')
+async def load():
+        bot.load_extension('C:\\Users\\yekut\\IdeaProjects\\Discord Bot\\venv\\cog\\hello.py')
+        await bot.tree.sync()
 
 @bot.command()
 @commands.is_owner()
 async def reload(ctx, extension):
     bot.reload_extension(f'cogs.{extension}')
     await ctx.send(f'Reloaded {extension}')
+    await self.tree.sync()
 
 bot.run(constants.botToken)
