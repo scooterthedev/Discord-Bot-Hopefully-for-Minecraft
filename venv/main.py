58f9e05693046e2a054ac02adadef020475b8489
Scooter <talkufy@outlook.com>
2024-07-09 23:10:54 -0400

    Added cog for auto re-loading and / commands

--- venv/main.py
+++ venv/main.py
@@ -1,24 +1,30 @@
-# This example requires the 'message_content' intent.
 import constants
 import discord
+from discord.ext import commands
+import os
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
 
-
-@client.event
+@bot.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    await bot.tree.sync(guild=discord.Object(id=constants.guildID))
+    print(f'Logged in as {bot.user}')
 
+@bot.tree.command(name="hello", description="Say hello to the bot", guild=discord.Object(id=constants.guildID))
+async def hello(interaction: discord.Interaction):
+    await interaction.response.send_message("Hello!")
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+for filename in os.listdir('cogs'):
+    if filename.endswith('.py'):
+        bot.load_extension(f'cogs.{filename[:-3]}')
 
-    if message.content.startswith('/hello'):
-        await message.channel.send('Hello!')
+@bot.command()
+@commands.is_owner()
+async def reload(ctx, extension):
+    bot.reload_extension(f'cogs.{extension}')
+    await ctx.send(f'Reloaded {extension}')
 
-client.run('BOT_TOKEN')
+bot.run(constants.botToken)
